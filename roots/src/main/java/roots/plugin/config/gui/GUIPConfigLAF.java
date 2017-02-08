545db9f393a0e97e60af74fe9904263b50a34b53
mnemotron <>
2017-02-08 22:54:15 +0100

    LAF

---
+++ roots/src/main/java/roots/plugin/config/gui/GUIPConfigLAF.java
@@ -0,0 +1,16 @@
+package roots.plugin.config.gui;
+
+import javax.swing.JPanel;
+
+public class GUIPConfigLAF extends JPanel
+{
+
+	/**
+	 * Create the panel.
+	 */
+	public GUIPConfigLAF()
+	{
+
+	}
+
+}
